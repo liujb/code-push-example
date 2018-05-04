63203bd52b2c43285b5a6d0382102153c66f70d7
LiuJiangbei <liujiangbei@didichuxing.com>
2018-05-04 18:53:12 +0800

    first add

---
+++ android/app/src/main/java/com/codepushexample/MainApplication.java
@@ -0,0 +1,51 @@
+package com.codepushexample;
+
+import android.app.Application;
+import com.microsoft.codepush.react.CodePush;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    public String getJSBundleFile() {
+        return CodePush.getJSBundleFile();
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+              new CodePush("fiCYhg0RGSqEIQ2X7xIk0pWAU9vH4ksvOXqog", MainApplication.this, BuildConfig.DEBUG, "http://10.96.112.165:9300")
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
